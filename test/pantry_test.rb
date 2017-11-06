ffdf1de1639bb214cb2fa242f1de68965c854246
NicholasMbogo <nicholasmbogo@yahoo.com>
2017-11-06 13:14:26 -0700

    created Pantry class

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,17 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
 
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_is_initialized_with_empty_hash
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_
 end
